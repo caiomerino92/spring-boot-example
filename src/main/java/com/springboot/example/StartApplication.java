d98496c8fac9715b9c8f028b29ea8fb5f871dee9
caio.merino <caio.merino@dextra-sw.com>
2020-08-23 22:38:38 -0300

    Adding some configurations and removing spring boot security dependency to not showing the login page

--- src/main/java/com/springboot/example/StartApplication.java
+++ src/main/java/com/springboot/example/StartApplication.java
@@ -2,8 +2,9 @@ package com.springboot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.springboot.example")
+@SpringBootApplication(scanBasePackages = "com.springboot.example", exclude = { SecurityAutoConfiguration.class })
 public class StartApplication {
 
     public static void main(String[] args) {
